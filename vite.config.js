eea21d873513d77134e6f0d290e11bf83b170227
RAKSHEDHA <rakshedhab@gmail.com>
2025-08-23 12:06:58 +0530

    Fix: Vite config for Vercel deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Change "my-portfolio" to your repo name
 export default defineConfig({
   plugins: [react()],
-  base: '/my-portfolio/',
 });
